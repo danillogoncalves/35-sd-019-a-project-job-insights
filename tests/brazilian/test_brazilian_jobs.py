d73ab226966f0bf93e8fe2acf8ce3500cd2ba949
danillogoncalves <=>
2022-10-23 17:49:45 -0300

    feat: :white_check_mark: Requisito 11 feito

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    test = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert test[0].get("title") is not None
+    assert test[0].get("salary") is not None
+    assert test[0].get("type") is not None
