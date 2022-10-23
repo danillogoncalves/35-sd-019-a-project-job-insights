fd825b012102281d004b56c5151c74cbb657156f
danillogoncalves <=>
2022-10-22 21:40:14 -0300

    feat: :white_check_mark: Requisito 10 feito.

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,20 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    results_of_words = {
+        "python": 1639,
+        "javascript": 122,
+    }
+    assert count_ocurrences("src/jobs.csv", "python") == results_of_words.get(
+        "python"
+    )
+    assert count_ocurrences(
+        "src/jobs.csv", "javascript"
+    ) == results_of_words.get("javascript")
+    assert count_ocurrences("src/jobs.csv", "PYTHON") == results_of_words.get(
+        "python"
+    )
+    assert count_ocurrences(
+        "src/jobs.csv", "jaVAscript"
+    ) == results_of_words.get("javascript")
